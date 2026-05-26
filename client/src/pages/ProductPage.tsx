0648e9a5cb03bf0b7fe194c7ced6e635c631a833
piyushsalunke2003 <piyushsalunke2003@gmail.com>
2026-05-26 18:34:15 +0530

    Working on individual Product Page

--- client/src/pages/ProductPage.tsx
+++ client/src/pages/ProductPage.tsx
@@ -1,9 +1,61 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { useCart } from "../context/CartContext";
+import type { Product } from "../types";
+import { dummyProducts } from "../assets/assets";
+import Loading from "../components/Loading";
+import { HomeIcon } from "lucide-react";
 
 
 const ProductPage = () => {
+
+  const currency = import.meta.env.VITE_CURRENCY_SYMBOL || "$";
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const {items, addToCart, updateQuantity, removeFromCart} = useCart()
+
+  const [product, setProduct] = useState<Product | null>(null)
+  const [relatedProducts, setRelatedProducts] = useState<Product[]>([])
+  const [loading, setLoading] = useState(true);
+  const [localQuantity, setLocalQuantity] = useState(1)
+
+  useEffect(()=>{
+    setLoading(true)
+    setLocalQuantity(1)
+    window.scroll(0,0)
+    const product = dummyProducts.find((p)=> p._id === id)
+    setProduct(product!)
+    setRelatedProducts(dummyProducts.filter((p)=> p._id !== id))
+    setLoading(false)
+
+  },[id, navigate])
+
+  if(loading) return <Loading />
+  if(!product) return null;
+
+  const cartItem = items.find((item)=> item.product._id === product._id)
+  const inCart = !!cartItem;
+  const displayQuantitiy = inCart ? cartItem.quantity : localQuantity
+
+  const categoryLabel = product.category.replace(/-/g, " ")
+
   return (
-    <div>
-      
+    <div className="min-h-screen">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+        {/* Breadcrumb */}
+        <nav className="flex items-center gap-2 text-sm text-app-text-light mb-6">
+          <Link to='/' className="hover:text-app-green transition-colors">
+            <HomeIcon className="size-4"/>
+          </Link>
+          <span>/</span>
+          <Link to='/products' className="hover:text-app-green transition-colors">
+            <HomeIcon className="size-4" />
+            Products
+          </Link>
+          <span>/</span>
+          <span className="text-app-green font-medium truncate max-w-[200px]">{product.name}</span>
+        </nav>
+      </div>
     </div>
   )
 }
