3862ae0aab7ef72270de7a67f2c8d1c65bd39f29
piyushsalunke2003 <piyushsalunke2003@gmail.com>
2026-06-03 12:03:01 +0530

    Working on Addresses page

--- client/src/pages/Addresses.tsx
+++ client/src/pages/Addresses.tsx
@@ -1,9 +1,60 @@
+import { useEffect, useState } from "react"
+import type { Address } from "../types"
+import { dummyAddressData } from "../assets/assets"
+import { PlusIcon } from "lucide-react"
 
 
 const Addresses = () => {
+
+  const [addresses, setAddresses] = useState<Address[]>([])
+  const [loading, setLoading] = useState(true)
+  const [showForm, setShowForm] = useState(false)
+  const [editingId, setEditingId] = useState<string | null>(null)
+  const [form, setForm] = useState({label: "",
+                                    address: "",
+                                    city: "",
+                                    state: "",
+                                    zip: "",
+                                    isDefault: false
+                                  });
+
+  const resetForm = ()=> {
+    setForm({label: "", address: "", city: "", state: "", zip: "", isDefault: false});
+    setShowForm(false)
+    setEditingId(null)
+  }
+
+  const handleSubmit = async (e:React.SubmitEvent) => {
+    e.preventDefault()
+  }
+
+  const onEditHandler = (add: Address)=> {
+    setForm({label: add.label,
+            address: add.address,
+            city: add.city,
+            state: add.state,
+            zip: add.zip,
+            isDefault: add.isDefault})
+    setEditingId(add._id)
+    setShowForm(true)
+  }
+
+  useEffect(()=>{
+    setAddresses(dummyAddressData)
+    setTimeout(()=>setLoading(false),1000)
+  },[])
+
   return (
-    <div>
-      
+    <div className="min-h-screen bg-app-cream">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        {/* page header */}
+        <div className="flex items-center justify-between mb-8">
+          <h1 className="text-2xl font-semibold text-app-green">My Addresses</h1>
+          <button onClick={()=> {resetForm(); setShowForm(true)}} className="px-4 py-2 bg-app-green text-white text-sm font-semibold rounded-xl hover:bg-app-green-light transition-colors flex items-center gap-2">
+            <PlusIcon className="size-4" /> Add Address
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
