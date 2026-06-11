9b0c0ce731dc52fd31f73944f917f871a5a0d7b3
piyushsalunke2003 <piyushsalunke2003@gmail.com>
2026-06-11 10:14:59 +0530

    Working on Search Results

--- client/src/pages/SearchResults.tsx
+++ client/src/pages/SearchResults.tsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react"
+import type { Product } from "../types"
+import { Link, useSearchParams } from "react-router-dom"
+import { dummyProducts } from "../assets/assets"
+import { Home } from "lucide-react"
 
 
 const SearchResults = () => {
+
+  const [products, setProducts] = useState<Product[]>([])
+  const [loading, setLoading] = useState(true)
+  const [searchParams] = useSearchParams()
+  const query = searchParams.get('q') || "";
+
+  useEffect(()=>{
+    if(!query) return;
+    setLoading(true)
+    setProducts(dummyProducts.filter((p: any)=> p.name.toLowerCase().includes(query.toLowerCase())))
+    setLoading(false)
+  },[query])
+
   return (
-    <div>
-      
+    <div className="min-h-screen bg-app-cream">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+        {/* Breadcrumb */}
+        <nav className="flex items-center gap-2 text-sm text-app-text-light mb-6">
+          <Link to='/' className="hover:text-app-green transition-colors">
+            <Home className="size-4"/>
+          </Link>
+          <span>/</span>
+          <span className="tetxt-app-green font-medium">Search Results</span>
+        </nav>
+      </div>
     </div>
   )
 }
